0484a91ae19ae13c3b9818728dbebe920ad4e4fb
Kevin Lacaille <kevinlacaille@gmail.com>
2019-04-08 10:32:57 -0400

    Not Boring Movies (easy)
    URL: https://leetcode.com/problems/not-boring-movies/
    
    Description: 
    X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
    
    Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.

---
+++ Not_boring_movies.sql
@@ -0,0 +1,15 @@
+# SQL Schema
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1))
+Truncate table cinema
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9')
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5')
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2')
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6')
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1')
+
+# MySQL query statement below
+SELECT *
+FROM cinema
+WHERE description != 'boring' #find all not boring movies
+AND id%2 = 1 #find only even ID movies
+ORDER BY rating DESC; #order movies by rating
