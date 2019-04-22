f55a52e3c3f6cc3fd488eeb686252f519b62bddf
Kevin Lacaille <kevinlacaille@gmail.com>
2019-04-22 15:29:12 -0400

    Length of Last Word (easy)
    URL: https://leetcode.com/problems/length-of-last-word/
    
    Description:
    Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
    
    If the last word does not exist, return 0.
    
    Note: A word is defined as a character sequence consists of non-space characters only.

---
+++ Length_of_last_word.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s.split() == []:
+            return 0
+        else:
+            return len(s.split()[-1])
