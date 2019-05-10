02493eb9095fbd668bc2fbbc5eb194785cb06d2b
Kevin Lacaille <kevinlacaille@gmail.com>
2019-05-10 11:34:07 -0400

    Peak Index in a Mountain Array (easy) - 2 options!
    URL: https://leetcode.com/problems/peak-index-in-a-mountain-array/
    
    Description:
    
    Let's call an array A a mountain if the following properties hold:
    
    - A.length >= 3
    - There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
    
    Given an array that is definitely a mountain, return any i such that A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].

--- Peak_index_in_a_mountain_array.py
+++ Peak_index_in_a_mountain_array.py
@@ -4,7 +4,10 @@ class Solution(object):
         :type A: List[int]
         :rtype: int
         """
-        # Simply finds the first "mountain" in mountain range
-        for i in xrange(len(A)):
-            if A[i] > A[i+1]:
-                return i
+        # Finds the tallest mountain in the mountain range
+        return A.index(max(A))
+
+        # ''''''Option 2: Simply finds the first "mountain" in mountain range''''''
+        # for i in xrange(len(A)):
+        #     if A[i] > A[i+1]:
+        #         return i
